cacd2add5cc5ed1d34231260e517cbb847fc783a
Techtress <47042355+Techtress@users.noreply.github.com>
2023-03-10 22:24:19 -0500

    Added socials

--- assets/AboutView-10d9e8ac.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-3d1c719d.js";const t={};function n(r,s){return c(),o("div",null," About ")}const a=e(t,[["render",n]]);export{a as default};
--- assets/ProjectsView-10b00387.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-3d1c719d.js";const r={};function t(n,s){return c(),o("div",null," Projects ")}const a=e(r,[["render",t]]);export{a as default};
