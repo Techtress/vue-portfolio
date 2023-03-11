caf185a31853e7f98c47bdcfe9e8d135d3d37d71
Techtress <47042355+Techtress@users.noreply.github.com>
2023-03-10 22:39:27 -0500

    testing new URL() with hard-coded image and v-bind

--- assets/AboutView-445c432c.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-5213e9a2.js";const t={};function n(r,s){return c(),o("div",null," About ")}const a=e(t,[["render",n]]);export{a as default};
---
+++ assets/ContactMeView-2bba812b.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as t}from"./index-8457331c.js";const n={};function o(r,a){return c(),t("div",null," Contact Me ")}const _=e(n,[["render",o]]);export{_ as default};
