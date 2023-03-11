72140cae5cdea025ea8ef122e3793cd644a78a6b
Techtress <47042355+Techtress@users.noreply.github.com>
2023-03-11 01:19:04 -0500

    (hopefully) fixing social icons not displaying

---
+++ assets/AboutView-6abd843b.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-253452b0.js";const t={};function n(r,s){return c(),o("div",null," About ")}const a=e(t,[["render",n]]);export{a as default};
--- assets/AboutView-ac3a0d98.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-8457331c.js";const t={};function n(r,s){return c(),o("div",null," About ")}const a=e(t,[["render",n]]);export{a as default};
