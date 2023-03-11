4af62cfad488f2ae0a27fae3faecfb985ff6ea34
Techtress <47042355+Techtress@users.noreply.github.com>
2023-03-11 01:31:32 -0500

    Attempt #3

---
+++ assets/AboutView-c532e459.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-105e56e1.js";const t={};function n(r,s){return c(),o("div",null," About ")}const a=e(t,[["render",n]]);export{a as default};
---
+++ assets/ProjectsView-46eac44e.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-105e56e1.js";const r={};function t(n,s){return c(),o("div",null," Projects ")}const a=e(r,[["render",t]]);export{a as default};
