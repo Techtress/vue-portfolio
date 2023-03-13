21b7a09b6a5bc260c6e7525a8b9603f80be4d883
Techtress <47042355+Techtress@users.noreply.github.com>
2023-03-13 14:26:42 -0400

    fixed header alignment and modified body

--- assets/AboutView-50d6e50f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-605fb87d.js";const t={};function n(r,s){return c(),o("div",null," About ")}const a=e(t,[["render",n]]);export{a as default};
---
+++ assets/AboutView-e47e349e.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-5963213b.js";const t={};function n(r,s){return c(),o("div",null," About ")}const a=e(t,[["render",n]]);export{a as default};
